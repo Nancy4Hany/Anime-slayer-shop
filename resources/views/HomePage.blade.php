debd7e2b4800de7def0c87696bd67a0a4b64cc59
Lojain Mostafa <lojain17mostafa@gmail.com>
2022-12-24 22:30:55 +0200

    Homepage

--- resources/views/HomePage.blade.php
+++ resources/views/HomePage.blade.php
@@ -59,17 +59,17 @@
     <section id="featuers">
         <div class="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row">
            {{-- What's Different --}}
-            <div class="flex flex-col space-y-12 md:1/2">
+        <div class="flex flex-col space-y-12 md:1/2">
            <h2 class="max-w-md text-4xl font-bold text-center md:text-left">
             What's Different About Anime Slayer?
            </h2>
            <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
             Animation in Japan began in the early 20th century, when filmmakers started to experiment with techniques pioneered in France, Germany, the United States, and Russia.
            </p>
-         </div>
+        </div>
          
           {{-- Number List --}}
-         <div class="flex flex-col space-y-8 md:w1/2">
+        <div class="flex flex-col space-y-8 md:w1/2">
              {{-- Item 1 --}}
              <div class="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
                {{-- Heading of items --}}
@@ -220,5 +220,63 @@
          </div>
         </div>
     </section>
+    {{-- Footer --}}
+    <footer class="bg-veryDarkBlue">
+       {{-- Flex Container --}}
+       <div class="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+        {{-- Logo and social links container  --}}
+        <div class="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+          {{-- Logo --}}
+          <div>
+             <img src="/img/logowhite (1).png" alt="logo" class="h-8">
+          </div>
+          {{-- Social Links Container --}}
+          <div class="flex justify-center space-x-4">
+             {{-- link 1 --}}
+             <a href="#"> 
+                <img src="/img/instagram.png" alt="facebook" class="h-8">
+             </a>
+             {{-- link 2 --}}
+             <a href="#"> 
+                <img src="/img/pinterest.png" alt="facebook" class="h-8">
+             </a>
+             {{-- link 3 --}}
+             <a href="#"> 
+                <img src="/img/facebook.png" alt="facebook" class="h-8">
+             </a>
+             {{-- link 4 --}}
+             <a href="#"> 
+                <img src="/img/twitter.png" alt="facebook" class="h-8">
+             </a>
+          </div>
+        </div>
+        {{-- List Container --}}
+        <div class="flex justify-round space-x-32 ">
+            <div class="flex flex-col space-y-3 text-white">
+              <a href="#" class="hover:text-brightRed">Home</a>
+              <a href="#" class="hover:text-brightRed">Pricing</a>
+              <a href="#" class="hover:text-brightRed">Products</a>
+              <a href="#" class="hover:text-brightRed">About Us</a>
+            </div>
+            <div class="flex flex-col space-y-3 text-white">
+                <a href="#" class="hover:text-brightRed">Trending</a>
+                <a href="#" class="hover:text-brightRed">Community</a>
+                <a href="#" class="hover:text-brightRed">Our Deals</a>
+              </div>
+        </div>
+        {{-- Input Container --}}
+        <div class="flex flex-col justify-between">
+        <form>
+            <div class="flex space-x-3" >
+            <input type="text" class="flex-1 px-4 rounded-full focus:outline-none" placeholder="Updates in your inbox">
+            <button class="px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none">
+                Go
+            </button>    
+        </div>
+            
+        </form>
+        </div>
+       </div>
+    </footer>
 </body>
 </html>
