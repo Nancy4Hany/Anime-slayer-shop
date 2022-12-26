9ca485e0153954ccd791fd547b8583c27e1f7a2a
70dakhaled <mahmoud2008975@miuegypt.edu.eg>
2022-12-26 12:30:11 +0200

    comment extend voyager

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends \TCG\Voyager\Models\User
+// class User extends \TCG\Voyager\Models\User
+class User
 {
     use HasApiTokens, HasFactory, Notifiable;
 
