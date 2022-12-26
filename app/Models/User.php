59c491f30420612a084bcd53ff13dab2992cb370
Lojain Mostafa <lojain17mostafa@gmail.com>
2022-12-26 13:14:20 +0200

    yalwhi

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-// class User extends \TCG\Voyager\Models\User
-class User
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
